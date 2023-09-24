13b08c2d4c86f34eb5b8a869fddd484136c641f5
joshy1912 <99416967+joshy1912@users.noreply.github.com>
2023-09-24 17:12:35 +0200

    fertig

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
+
+        String[] questions = {
+            "",
+            "",
+            "",
+        };
+
+        String[] answers = {
+                "",
+                "",
+                "",
+        };
+        int numberOfQuestions = questions.length;
+
+        for (int i = 0; i< numberOfQuestions; i++){
+            System.out.println("question " + (i + 1)+ ": " + questions[i]);
+            System.out.println("your answer: ");
+            String userAnswer = scanner.nextLine();
+
+
+            if (userAnswer.equalsIgnoreCase(answers[i])){
+                System.out.println("Correct!\n");
+                score++;
+            }else {
+                System.out.println("Wrong the correct answer is: " + answers[i] + "\n");
+            }
+        }
+        System.out.println("Quiz completed!");
+        System.out.println("Your score:"+ score + "out of " + numberOfQuestions);
+
+        scanner.close();
     }
+
+
+
+
+
 }
